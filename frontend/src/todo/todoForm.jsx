20da2e52e6ad4087600ed39d3e68465ea339d401
Wagner Almeida <wdmeida@gmail.com>
2017-05-16 17:39:14 -0300

    Implementação do CRUD das tarefas.

--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -2,16 +2,31 @@ import React from 'react'
 import Grid from '../template/grid'
 import IconButton from '../template/iconButton'
 
-export default props => (
-    <div role='form' className='todoForm'>
+export default props => {
+    const keyHandler = (e) => {
+        if (e.key === 'Enter') {
+            e.shiftKey ? props.handleSearch() : props.handleAdd()
+        } else if (e.key === 'Escape') {
+            props.handleClear()
+        }
+    }
 
-        <Grid cols='12 9 10'>
-            <input id="description" className='form-control' placeholder='Adiciona uma tarefa' 
-                   onChange={props.handleChange} value={props.description}/>
-        </Grid>
+    return (
+        <div role='form' className='todoForm'>
 
-        <Grid cols='12 3 2'>
-            <IconButton style='primary' icon='plus' onClick={props.handleAdd}></IconButton>
-        </Grid>
-    </div>
-)
+            <Grid cols='12 9 10'>
+                <input id="description" className='form-control' 
+                    placeholder='Adiciona uma tarefa' 
+                    onChange={props.handleChange}
+                    onKeyUp={keyHandler} 
+                    value={props.description}/>
+            </Grid>
+
+            <Grid cols='12 3 2'>
+                <IconButton style='primary' icon='plus' onClick={props.handleAdd}></IconButton>
+                <IconButton style='info' icon='search' onClick={props.handleSearch} ></IconButton>
+                <IconButton style='default' icon='close' onClick={props.handleClear} ></IconButton>
+            </Grid>
+        </div>
+    )
+}
