6f2839cb21a6d7fa33776436986e72bcf294a3c9
Wagner Almeida <wdmeida@gmail.com>
2017-05-16 10:22:48 -0300

    Implementado state de descrição de tarefa.

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -5,11 +5,30 @@ import TodoForm from './todoForm'
 import TodoList from './todoList'
 
 export default class Todo extends Component {
+    constructor(props) {
+        super(props)
+        this.state = { description: '', list: [] }
+
+        // Garante que o independente do local de chamada, this referênciará o componente original.
+        this.handleChange = this.handleChange.bind(this)
+        this.handleAdd = this.handleAdd.bind(this)
+    }
+
+    handleChange(e) {
+        this.setState({...this.state, description: e.target.value })
+    }
+
+    handleAdd() {
+        console.log(this.state.description)
+    }
+
     render() {
         return (
             <div>
                 <PageHeader name='Tarefas' small='Cadastro'></PageHeader>
-                <TodoForm />
+                <TodoForm description={this.state.description} 
+                    handleChange={this.handleChange} 
+                    handleAdd={this.handleAdd} />
                 <TodoList />
             </div>
         )
--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -6,11 +6,12 @@ export default props => (
     <div role='form' className='todoForm'>
 
         <Grid cols='12 9 10'>
-            <input id="description" className='form-control' placeholder='Adiciona uma tarefa' />
+            <input id="description" className='form-control' placeholder='Adiciona uma tarefa' 
+                   onChange={props.handleChange} value={props.description}/>
         </Grid>
 
         <Grid cols='12 3 2'>
-            <IconButton style='primary' icon='plus'></IconButton>
+            <IconButton style='primary' icon='plus' onClick={props.handleAdd}></IconButton>
         </Grid>
     </div>
 )
